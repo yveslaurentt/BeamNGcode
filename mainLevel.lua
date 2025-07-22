cef0a8f86787f2408a7297058da6de5084fb32af
yveslaurentt <shawndav1027@gmail.com>
2025-07-22 13:14:34 -0500

    mainLevel.lua
    traffic vars for a singular map

--- mainLevel.lua
+++ mainLevel.lua
@@ -9,15 +9,17 @@ function onTrafficStarted()
  local vars =
   { baseAggression = 0.5, -- any number between 0.2 and 2
     speedLimit = nil, -- can be any number between -1 (off or nil, will use the road's speed limit), 100 -- (360 km/h) also beware, some cars can't reach that speed
-    spawnValue = 0.75 -- basically the cars spawn rate
-  log("I", "trafficVars", "baseAggression =" .. vars.speedLimit .. ", speedLimit = " .. vars.speedLimit) -- shows values in the console
+    spawnValue = 0.75 -- basically the cars spawn rate, any number between 0 to 3
+  } -- if you dont end vars table it wont work obviously, oh and put the vars table first! lua works in order.
+  gameplay_traffic.setTrafficVars(vars) -- it sets the vars in the vars table
+  log("I", "trafficVars", "baseAggression =" .. vars.baseAggression .. ", speedLimit = " .. vars.speedLimit) -- shows values in the console
 end
 -- module table, 
 M.onTrafficStarted = onTrafficStarted 
 M.onLoad = onLoad
 
 return M
-  -- if you wanna do more traffic vars setting, you can use these too! straight from beamng's traffic lua
+  -- if you wanna do more traffic vars setting, you can use these too! straight from BeamNG's traffic lua
   --[[
   vars = {
     spawnValue = 1, -- as the default value, globalSpawnDist will dynamically adjust the random respawn distance from player
